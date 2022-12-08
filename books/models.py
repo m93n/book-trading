f3ce95adfd54386ff1313d4f8628c0476f3204f9
m93n <mohsen.k9731@gmail.com>
2022-12-08 14:44:13 +0330

    delete and comment future updates

--- books/models.py
+++ books/models.py
@@ -7,53 +7,54 @@ class Book(models.Model):
     image = models.ImageField(upload_to="images/")
     name = models.CharField(max_length=30)
     introduction = models.TextField()
-    populer_tag = models.BooleanField()
-    hashtag = models.ManyToManyField('HashTag', blank=True)
 
     def __str__(self):
         return self.name
 
 
-class UserBook(models.Model):
-    TRADING_TYPE = [
-        ("sell", "sell"),
-        ("lend", "lend"),
-        ("exchange", "exchange"),
-        ("donate", "donate"),
-    ]
 
-    TAG = [
-        ("I have","I have"),
-        ("I am reading","I am reading"),
-        ("I will read","I will read"),
-        ("I read","I read"),
-    ]
-    user         = models.ForeignKey(User_Model, on_delete=models.CASCADE)
-    book         = models.ForeignKey(Book, on_delete=models.CASCADE)
-    new          = models.BooleanField()
-    trading_type = models.CharField(max_length=10, choices=TRADING_TYPE, null=True, blank=True)
-    tag          = models.CharField (max_length=12, choices=TAG, default='I read')
-    status       = models.ForeignKey('BookStatus', on_delete=models.CASCADE, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_updated =  models.DateTimeField(auto_now=True)
+#// FUTURE UPDATES
 
-    def __str__(self):
-        return self.book
+# class UserBook(models.Model):
+#     TRADING_TYPE = [
+#         ("sell", "sell"),
+#         ("lend", "lend"),
+#         ("exchange", "exchange"),
+#         ("donate", "donate"),
+#     ]
 
-class UserBookImage(models.Model):
-    name = models.CharField(max_length=50)
-    book = models.ForeignKey(UserBook, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="images/")
-    default = models.BooleanField(default=False)
+#     TAG = [
+#         ("I have","I have"),
+#         ("I am reading","I am reading"),
+#         ("I will read","I will read"),
+#         ("I read","I read"),
+#     ]
+#     user         = models.ForeignKey(User_Model, on_delete=models.CASCADE)
+#     book         = models.ForeignKey(Book, on_delete=models.CASCADE)
+#     new          = models.BooleanField()
+#     trading_type = models.CharField(max_length=10, choices=TRADING_TYPE, null=True, blank=True)
+#     tag          = models.CharField (max_length=12, choices=TAG, default='I read')
+#     status       = models.ForeignKey('BookStatus', on_delete=models.CASCADE, null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     last_updated =  models.DateTimeField(auto_now=True)
 
-class HashTag(models.Model):
-    name = models.CharField(max_length=30)
+    # def __str__(self):
+    #     return self.book
 
-class BookStatus(models.Model):
-    STATUS_TYPE = [
-        ("sold","sold"),
-        ("borrowed","borrowed"),
-        ("loaned","loaned"),
-    ]
-    status = models.CharField(max_length=15, choices=STATUS_TYPE)
-    status_date = models.DateField()
+# class UserBookImage(models.Model):
+#     name = models.CharField(max_length=50)
+#     book = models.ForeignKey(UserBook, on_delete=models.CASCADE)
+#     image = models.ImageField(upload_to="images/")
+#     default = models.BooleanField(default=False)
+
+# class HashTag(models.Model):
+#     name = models.CharField(max_length=30)
+
+# class BookStatus(models.Model):
+#     STATUS_TYPE = [
+#         ("sold","sold"),
+#         ("borrowed","borrowed"),
+#         ("loaned","loaned"),
+#     ]
+#     status = models.CharField(max_length=15, choices=STATUS_TYPE)
+#     status_date = models.DateField()
--- users/models.py
+++ users/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from django.conf import settings
 
-from books.models import Book, HashTag
+from books.models import Book
 
 User_Model = settings.AUTH_USER_MODEL
 
-class UserInfo(models.Model):
-    user = models.OneToOneField(User_Model, on_delete=models.CASCADE)
-    about_me = models.TextField()
-    image = models.ImageField()
-    followers = models.ManyToManyField(User_Model, related_name='userinfo_followers')
-    following = models.ManyToManyField(User_Model, related_name='userinfo_following')
+#// FUTURE UPDATES
 
-class Post(models.Model):
-    user = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    text = models.TextField()
-    src = models.ImageField()
-    hashtag = models.ManyToManyField(HashTag, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_updated =  models.DateTimeField(auto_now=True)
+# class UserInfo(models.Model):
+#     user = models.OneToOneField(User_Model, on_delete=models.CASCADE)
+#     about_me = models.TextField()
+#     image = models.ImageField()
+#     followers = models.ManyToManyField(User_Model, related_name='userinfo_followers')
+#     following = models.ManyToManyField(User_Model, related_name='userinfo_following')
+
+# class Post(models.Model):
+#     user = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     src = models.ImageField()
+#     hashtag = models.ManyToManyField(HashTag, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     last_updated =  models.DateTimeField(auto_now=True)
     
