bdf26e2e43c820f7651b2ed196c02567c2527e2a
m93n <mohsen.k9731@gmail.com>
2022-12-09 18:34:59 +0330

    add book detail view

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from books import views
 
 urlpatterns = {
-    path('list/', views.BookList.as_view())
+    path('list/', views.BookList.as_view()),
+    path('detail/<int:pk>', views.BookDetail.as_view()),
 }
